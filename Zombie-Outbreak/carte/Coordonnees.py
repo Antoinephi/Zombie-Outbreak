da66f93f391ee374981e97e0d8453199b8d411e0
Mandrivia <antoine.philippe62@gmail.com>
2013-01-24 17:51:59 +0100

    création de la classe Coordonnees

---
+++ Zombie-Outbreak/carte/Coordonnees.py
@@ -0,0 +1,17 @@
+'''
+Created on 24 janv. 2013
+
+@author: Antoine
+'''
+class Coordonnees:
+    
+    def __init__(self,x, y):
+        self.x = x
+        self.y = y
+        
+        
+    def _getX_(self):
+        return self.x
+   
+    def _getY_(self):
+        return self.y
